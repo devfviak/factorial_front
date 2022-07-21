57e76e762905ce1812f09811da06480a394aac1e
Pedro Ávila <pedro.avila@cleverppc.com>
2022-07-21 15:56:19 +0200

    feat: Register page (todo)

---
+++ src/pages/register/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import BasicLayout from '../../components/layout/BasicLayout';
+
+function RegisterPage () {
+  return (
+    <BasicLayout isRegister >
+      REGISTER
+    </BasicLayout>
+  );
+}
+
+export default RegisterPage;
