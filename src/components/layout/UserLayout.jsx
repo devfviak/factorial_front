da75aac7e53fb262a3faa3c8ec0c6977f530318f
Pedro Ávila <pedro.avila@cleverppc.com>
2022-07-22 00:51:39 +0200

    fix(UserLayout): Enable vertical grow on layout

--- src/components/layout/UserLayout.jsx
+++ src/components/layout/UserLayout.jsx
@@ -5,15 +5,15 @@ import UserNavbar from './UserNavbar';
 
 function UserLayout (props) {
   return (
-    <div className="bg-factorial-gray">
-      <div className='py-4 flex flex-row max-w-5xl h-screen mx-auto bg-factorial-gray'>
+    <div className="bg-factorial-gray min-h-screen">
+      <div className='py-4 flex flex-row max-w-5xl h-full mx-auto bg-factorial-gray'>
         { /* Sidebar */ }
         <div className='w-1/5 mr-5'>
           <UserNavbar/>
         </div>
 
         { /* Page content */ }
-        <main className='w-full py-4 flex flex-grow'>
+        <main className='w-full py-4 px-4 flex flex-grow'>
           {props.children}
         </main>
       </div>
