ee8609b56eb9193a30f1ab111d3cf8f46ec28648
Pedro Ávila <pedro.avila@cleverppc.com>
2022-07-22 04:38:56 +0200

    feat(TextInput): Include error alerts

--- src/components/forms/TextInput.jsx
+++ src/components/forms/TextInput.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 
-function TextInput ({ value, onChange, inputId, type, required, maxLength, labelName }) {
+function TextInput ({ value, onChange, inputId, type, required, maxLength, labelName, error, disabled = false }) {
   return (
     <div className='relative w-full'>
       <input
@@ -9,13 +9,14 @@ function TextInput ({ value, onChange, inputId, type, required, maxLength, label
         type={type}
         required={required}
         maxLength={maxLength }
-        className='h-16 w-full rounded-md p-3 pt-8 peer border-gray-500 border-2 focus:border-factorial-green outline-none'
+        className={`h-16 w-full rounded-md p-3 pt-8 peer ${error ? 'border-red-400' : 'border-gray-500'} border-2 focus:border-factorial-green outline-none`}
         value={value}
         onChange={onChange}
+        disabled={disabled}
       />
       <label htmlFor={inputId}
-        className='text-sm font-semibold absolute top-3 left-3 text-gray-500'>
-        {labelName}
+        className={`flex flex-row justify-between text-sm font-semibold absolute top-3 left-3 ${error ? 'text-red-400' : 'text-gray-500'} `}>
+        {labelName}{ error && ` ${error}`}
       </label>
     </div>
   );
